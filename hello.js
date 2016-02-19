be869b6cc9c403bd668a52bc71cae1f5e27f2d02
sarah <sarahomar995@gmail.com>
2016-02-19 19:54:30 +0200

    Added Comment

--- hello.js
+++ hello.js
@@ -3,3 +3,11 @@ console.log("Hello World");
 var print = function (word) {
     console.log(word)
 }
+// hello.js
+
+/**
+ * print a word the console
+ */
+var print = function (word) {
+    console.log(word)
+}
